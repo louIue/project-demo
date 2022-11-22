700a3dc7601a1c01ce5485dc34e7651f23c191fe
louie <louie68612@gmail.com>
2022-11-22 23:07:44 +0700

    stateusefull

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,50 +1,64 @@
 import React from "react";
 import './DisplayInfo.scss';
 import logo from '../logo.svg'
-class DisplayInfo extends React.Component {
 
-    state = {
-        isShowListUser: true
-    }
+// class DisplayInfo extends React.Component {
 
-    handleShowHide = () => {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
+//     render() {
+//         const { listUser } = this.props;
+//         return (
+//             <div className='display-info-container'>
+//                 <img src={logo} />
+//                 {true &&
+//                     <>
+//                         {listUser.map((user) => {
+//                             return (
+//                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                                     <div>
+//                                         My name {user.name}
+//                                         <br />
+//                                         My age {user.age}
+//                                         <hr />
+//                                     </div>
+//                                     <div>
+//                                         <button onClick={() => { this.props.handleDeleteUser(user.id) }}>Delete</button>
+//                                     </div>
+//                                 </div>
+//                             )
+//                         })}
+//                     </>
+//                 }
+//             </div>
+//         )
+//     }
+// }
 
-    render() {
-        const { listUser } = this.props;
-        return (
-            <div className='display-info-container'>
-                <img src={logo} />
-                <div>
-                    <span onClick={() => { this.handleShowHide() }}>
-                        {this.state.isShowListUser === true ? "Hide list users" : "Show list users"}
-                    </span>
-                </div>
-                {this.state.isShowListUser &&
-                    <>
-                        {listUser.map((user) => {
-                            return (
-                                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                    <div>
-                                        My name {user.name}
-                                        <br />
-                                        My age {user.age}
-                                        <hr />
-                                    </div>
-                                    <div>
-                                        <button onClick={()=>{this.props.handleDeleteUser(user.id)}}>Delete</button>
-                                    </div>
+const DisplayInfo = (props) => {
+    const { listUser } = props;
+    return (
+        <div className='display-info-container'>
+            <img src={logo} />
+            {true &&
+                <>
+                    {listUser.map((user) => {
+                        return (
+                            <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                <div>
+                                    My name {user.name}
+                                    <br />
+                                    My age {user.age}
+                                    <hr />
                                 </div>
-                            )
-                        })}
-                    </>
-                }
-            </div>
-        )
-    }
+                                <div>
+                                    <button onClick={() => { props.handleDeleteUser(user.id) }}>Delete</button>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </>
+            }
+        </div>
+    )
 }
 
 export default DisplayInfo;
